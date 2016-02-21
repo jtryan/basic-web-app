18a4d73a70c10049046d70d7fd39fda896f47c25
John T. Ryan <uijedi@gmail.com>
2016-02-21 15:56:16 -0500

    skeleton complete

---
+++ src/main/java/tutorial/mvc/TestController.java
@@ -0,0 +1,16 @@
+package tutorial.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by john on 2/21/16.
+ */
+@Controller
+public class TestController {
+    @RequestMapping("/test")
+    public String test()
+    {
+        return "view";
+    }
+}
